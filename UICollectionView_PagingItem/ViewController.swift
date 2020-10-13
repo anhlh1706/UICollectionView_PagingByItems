251078233d825f20ec14b53a43de9246ce93d3e5
Hoang Anh <anh.le@fujitechjsc.com>
2020-10-13 10:43:19 +0700

    Initial Commit

---
+++ UICollectionView_PagingItem/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UICollectionView_PagingItem
+//
+//  Created by Lê Hoàng Anh on 13/10/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
